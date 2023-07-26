5c89970c993e113a569e5ae471cdfd8fe9fa790c
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-07-26 21:46:04 +0530

    Delete Python Coding Problems directory

--- Python Coding Problems/defanging-an-ip-address.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def defangIPaddr(self, address: str) -> str:
-        string = address.split(".")
-        
-        letters = ""
-        for i in range(len(string)):
-            letters +=  string[i] + "[.]"
-        return(letters.strip("[.]"))
