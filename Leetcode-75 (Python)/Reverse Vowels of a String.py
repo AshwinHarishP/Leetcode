6899b622aae64f57b0b1596093a416f74d3df269
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-07-31 22:20:19 +0530

    Create Reverse Vowels of a String.py

---
+++ Leetcode-75 (Python)/Reverse Vowels of a String.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = set("aeiouAEIOU")
+        i = 0
+        j = len(s) - 1
+        s_list = list(s)
+
+        while i < j:
+            while i < j and s_list[i] not in vowels:
+                i += 1
+            while i < j and s_list[j] not in vowels:
+                j -= 1
+
+            if i < j:
+                s_list[i], s_list[j] = s_list[j], s_list[i]
+                i += 1
+                j -= 1
+
+        return "".join(s_list)
