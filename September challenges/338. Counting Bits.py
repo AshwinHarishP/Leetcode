8e07969858a2c77b21d008d1e49e0727806a06a7
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-09-11 00:31:19 +0530

    Create 338. Counting Bits.py

---
+++ September challenges/338. Counting Bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        lst=[]
+        for x in range(0,n+1):
+            if x>1:
+                lst.append(lst[x//2]+(x%2))
+            else:
+                lst.append(x)
+        return lst
