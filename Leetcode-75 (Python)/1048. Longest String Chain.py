de22618c1cefd9c3eb6d66d9d458519d7fed1810
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-09-23 10:57:32 +0530

    Create 1048. Longest String Chain.py

---
+++ Leetcode-75 (Python)/1048. Longest String Chain.py
@@ -0,0 +1,15 @@
+import collections
+
+
+class Solution(object):
+    def longestStrChain(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        words.sort(key=len)
+        dp = collections.defaultdict(int)
+        for w in words:
+            for i in xrange(len(w)):
+                dp[w] = max(dp[w], dp[w[:i]+w[i+1:]]+1)
+        return max(dp.itervalues())
