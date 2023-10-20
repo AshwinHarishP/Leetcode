44221b04b085ddf6488edba6092b722135713eb9
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-10-20 09:07:56 +0530

    Create Missing Number.py

---
+++ Coding Problems - Python/Arrays/Missing Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            correct = nums[i]
+            if nums[i] < len(nums) and nums[i] != nums[correct]:
+                nums[i],nums[correct] = nums[correct],nums[i]
+            else:
+                i += 1
+            
+        for i in range(len(nums)):    
+            if i != nums[i]:
+                return i
+
+        return len(nums)
+
+        
